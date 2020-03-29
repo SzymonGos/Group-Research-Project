86f886744dea3c5271250f6e81f5d3dac9107710
Szymon Gos <37188746+SzymonGos@users.noreply.github.com>
2020-03-29 10:59:47 +0100

    Add files via upload

---
+++ BeerBelly/script.js
@@ -0,0 +1,7 @@
+document.getElementById("login").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "flex";
+})
+
+document.querySelector(".close").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "none";
+})
