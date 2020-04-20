155130a37ae4b311f5f2b9769f61d525eaac7561
Szymon Gos <37188746+SzymonGos@users.noreply.github.com>
2020-04-20 22:15:31 +0200

    Add files via upload

---
+++ BeerBelly/BeerBelly/javascripts/checkout.js
@@ -0,0 +1,8 @@
+function checkOut() {
+    var page = document.getElementById("payment-page");
+    var button = document.getElementById('btn-hide');
+    if (page.style.display = "none") {
+        page.style.display = "block";
+        button.style.display = "none";
+    }
+}
