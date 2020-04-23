bacfbb5cfcbfbf3e740aa72e747859027dc83045
Szymon Gos <37188746+SzymonGos@users.noreply.github.com>
2020-04-23 10:00:41 +0200

    Delete checkout.js

--- BeerBelly/BeerBelly/javascripts/checkout.js
+++
@@ -1,8 +0,0 @@
-function checkOut() {
-    var page = document.getElementById("payment-page");
-    var button = document.getElementById('btn-hide');
-    if (page.style.display = "none") {
-        page.style.display = "block";
-        button.style.display = "none";
-    }
-}
